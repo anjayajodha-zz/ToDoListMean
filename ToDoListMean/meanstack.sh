2f94e5efe0858187a896a6b5450d24a295a26cc6
Anjay Ajodha <anjay@anjay.me>
2016-05-24 02:14:40 -0700

    Updating for new demo.

--- ToDoListMean/meanstack.sh
+++ ToDoListMean/meanstack.sh
@@ -2,19 +2,19 @@
 #Script runs as root, be careful!
 #Install node.js
 
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
-apt-get -qq install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_4.x | bash -
+#apt-get -qq install -y nodejs
 
 #Install Git
-apt-get -qq install -y git
+#apt-get -qq install -y git
 
 #Install MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
-apt-get -qq update
+#apt-get -qq update
 
-apt-get -qq install -y mongodb-org
+#apt-get -qq install -y mongodb-org
 
 service mongod start
 
